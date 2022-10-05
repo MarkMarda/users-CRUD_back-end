0a5c2a7db16c4b64d84e780d0590013718a17cc5
Marda <marda_da-capo@hotmail.com>
2022-10-05 03:07:57 -0500

    initialized db, mvc model based

---
+++ src/users/users.controllers.js
@@ -0,0 +1,49 @@
+const uuid = require("uuid");
+
+const usersDB = [
+  {
+    id: "002dee28-8916-4cf3-92a9-65ea1d51042b",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    birthday: ""
+  },
+  {
+    id: "5468677d-f9f5-4598-b56c-be8231ef41a4",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    birthday: ""
+  },
+  {
+    id: "517c67d7-2f8a-45fa-a40e-ad36b677364e",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    birthday: ""
+  },
+  {
+    id: "1860a949-86b5-40c7-bab6-8b6fa3f2daf7",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    birthday: ""
+  },
+  {
+    id: "d1f07b53-8b04-4648-b595-9ff731a00e31",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    birthday: ""
+  },
+];
+
+const getAllUsers = () => {
+  return usersDB;
+};
+
